e03ca04db03f5e274b9696b2aacd8265efee626c
FZubietaC <fernando.zubieta@finanzauto.com.co>
2022-06-22 10:40:58 -0500

    cambios

---
+++ CursoJavascript/funciones.js
@@ -0,0 +1,14 @@
+//Declarativas
+
+function Test1(){
+    return 3;
+}
+
+Test1();
+//Expresión
+
+var Test = function(a,b){
+    return a + b;
+}
+
+Test();
---
+++ CursoJavascript/valores.js
@@ -0,0 +1,12 @@
+// declaración del variables
+
+var edad;
+
+var elementos =["pc","celu"];
+
+var persona = {
+    nombre: "Diego",
+    edad: 30
+}
+
+
