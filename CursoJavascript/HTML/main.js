b84f354762fb9d122dd3ca8bf2201c6a3f70525a
Fernando Zubieta Cortes <104217237+FZubietaC@users.noreply.github.com>
2023-03-13 17:03:32 -0500

    agregado_menus

--- CursoJavascript/HTML/main.js
+++ CursoJavascript/HTML/main.js
@@ -1,9 +1,17 @@
 const navEmail = document.querySelector('.nav-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const iconMenu = document.querySelector('.menu');
+const navMobile = document.querySelector('.mobile-menu');
+
 
 navEmail.addEventListener('click',toogleDesktopMenu);
+iconMenu.addEventListener('click',toogleMobileMenu);
 
 function toogleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+
+function toogleMobileMenu() {
+    navMobile.classList.toggle('inactive');
 }
 
